9f3d25eafcac0f605bd8e293342c22f038f29a5c
Matt <mateuszmusial95@gmail.com>
2026-01-24 17:37:09 +0100

    <3GPP Search assistant>: Added UTs
    * Added more Uts for pdf_processor module.

--- tests/test_pdf_processor.py
+++ tests/test_pdf_processor.py
@@ -2,6 +2,7 @@ import pytest
 from pathlib import Path
 from pytest_mock import MockerFixture
 from document_processors.pdf_processor import PDFProcessor
+from langchain_core.documents.base import Document
 
 
 
@@ -50,3 +51,41 @@ def test_get_3gpp_document_path_no_pdfs(mocker: MockerFixture) -> None:
 
     mock_exist.assert_called_once()
     mock_glob.assert_called_once()
+
+
+def test_load_document_success(mocker) -> None:
+    """
+    Test whether load_document successfully loads a PDF document.
+    """
+    # Arrange
+    mock_loader_class = mocker.patch("document_processors.pdf_processor.PyMuPDFLoader")
+    expected_docs = [Document(page_content="test", metadata={})]
+    mock_loader_class.return_value.load.return_value = expected_docs
+    loger_mock = mocker.patch("document_processors.pdf_processor.logger")
+    pdf_processor = PDFProcessor()
+
+    # Act
+    document = pdf_processor.load_document("data/test.pdf")
+
+    # Assert
+    assert document == expected_docs
+    mock_loader_class.assert_called_once_with("data/test.pdf")
+    mock_loader_class.return_value.load.assert_called_once()
+    loger_mock.info.assert_called_once_with("Document loaded successfully from data/test.pdf.")
+
+def test_load_document_failure(mocker) -> None:
+
+    TEST_FILE_PATH = "data/test.pdf"
+    # Arrange
+    mock_loader_class = mocker.patch("document_processors.pdf_processor.PyMuPDFLoader")
+    mock_loader_class.side_effect = Exception(f"Failed to load document from {TEST_FILE_PATH}")
+    loger_mock = mocker.patch("document_processors.pdf_processor.logger")
+    pdf_processor = PDFProcessor()
+
+    # Act
+    result = pdf_processor.load_document(TEST_FILE_PATH)
+
+    # Assert
+    assert result is None
+    mock_loader_class.assert_called_once_with("data/test.pdf")
+    loger_mock.error.assert_called_once_with(f"Failed to load document from {TEST_FILE_PATH}: Failed to load document from {TEST_FILE_PATH}")
