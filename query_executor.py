3fe7fffcf8a6050cc10f31bf53a52bf17dede2b9
Matt <mateuszmusial95@gmail.com>
2026-01-25 12:13:14 +0100

    <3GPP Search assistant>: Bugfix
    * Fixed bug in query_executor_module

--- query_executor.py
+++ query_executor.py
@@ -1,17 +1,14 @@
 import logging
-from dotenv import load_dotenv
 
 from rag_pipeline import start_rag_pipeline
 from model_service import create_llm_model
 from document_processors.document_processor import DocumentProcessor
-from document_processors.pdf_processor import PDFProcessor
 
 logger = logging.getLogger(__name__)
 
 class QueryExecutor:
     def __init__(self, file_processor: DocumentProcessor):
-        load_dotenv()
-        self.file_processor = PDFProcessor()
+        self.file_processor = file_processor
         self.vectorstore = start_rag_pipeline(file_processor=self.file_processor)
         self.model = create_llm_model()
 
